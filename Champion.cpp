3a4128ee1e6fb0e8113c907fc151b782528e1a20
Orimol <33389705+Orimol@users.noreply.github.com>
2018-05-23 19:03:33 +0300

    Add files via upload

--- Champion.cpp
+++ Champion.cpp
@@ -2,20 +2,29 @@
 
 
 const Coordinate Champion::play(const Board& board) {
-
-	static Coordinate c{0, board.size()};
-	static Coordinate d{board.size(), 0};
-	 Coordinate zero{0,0};
-	 Coordinate zo{0,1};
-	 Coordinate oz{1,0};
-	size_t counter=0;
-	size_t count_down=board.size();
-	if (board[zero]=='.') return c;
-	if (board[zero]!='.' && board[oz]=='.' && board[zo]=='.') return c;
-	if (board[zero]!='.'){
-		Coordinate d{count_down, counter};
-		counter++;
-		count_down--;
+	Coordinate c{board.size()-1, 0};
+	Coordinate d{0, board.size()-1};
+	if (board[c]=='.') return c;
+	if (board[d]=='.') return d;
+	for (uint i=1; i<board.size(); i++){
+		Coordinate	ce{i, board.size()-1}	;
+		if (board[ce]=='.') return ce;
 	}
-	return d;
+
+
+//	static Coordinate c{0, board.size()-1};
+//	static Coordinate d{board.size()-1, 0};
+//	 Coordinate zero{0,0};
+//	 Coordinate zo{0,1};
+//	 Coordinate oz{1,0};
+//	size_t counter=0;
+//	size_t count_down=board.size()-1;
+//	if (board[zero]=='.') return c;
+//	if (board[zero]!='.' && board[oz]=='.' && board[zo]=='.') return c;
+//	if (board[zero]!='.'){
+//		Coordinate d{count_down, counter};
+//		counter++;
+//		count_down--;
+//	}
+//	return d;
 }
